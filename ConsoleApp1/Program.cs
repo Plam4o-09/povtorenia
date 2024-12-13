3b6bc0a10dd6ca30db633bb63a88f0bce279a152
Plam4o-09 <plamenduralijski@gmail.com>
2024-12-13 09:09:24 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            LinkedList<int> list = new LinkedList<int>(nums);
+
+            LinkedListNode<int> node = list.First;
+            while (node != null) 
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+                list.Remove(node);
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string ime;
+            //Console.ReadLine(Kak se kazva6);
+            //Console.ReadLine();
+            Console.Beep(432, 500);
+            for (int i = 200; i <= 4000; i += 200)
+                Console.Beep(i, 100);
+            Console.WriteLine("Hello,");
+            Console.WriteLine((3522+52353)*23-(2336*501+23432-6743)*3);
+
+        }
+    }
+}
